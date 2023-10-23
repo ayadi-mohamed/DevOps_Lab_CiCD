77e58c02787c774eecbfc585b1d9bcfa2eb36fc5
Ayadi Mohamed <ayadi.01.mohamed@gmail.com>
2023-10-23 19:09:45 +0100

    Update sonarScanner.sh

--- sonarScanner.sh
+++ sonarScanner.sh
@@ -1,4 +1,4 @@
 mvn clean verify sonar:sonar \
   -Dsonar.projectKey=spring-project \
-  -Dsonar.host.url=http://192.168.122.144:9000 \
+  -Dsonar.host.url=http://172.24.0.3:9000 \
   -Dsonar.login=sqp_57aeff21d5ada3916b2d5748e6ca4071bf319570
